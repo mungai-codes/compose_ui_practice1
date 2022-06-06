5468c8c6c201e6c82da026166621bd51808f431e
mmungai <mungai_official@outlook.com>
2022-06-06 10:56:43 +0300

    Initial commit

---
+++ app/src/main/java/com/mungaicodes/taskmanager/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.mungaicodes.taskmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.mungaicodes.taskmanager.ui.theme.TaskManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TaskManagerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    TaskManagerApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskManagerApp() {
+    TaskManagerAppDetails()
+}
+
+@Composable
+fun TaskManagerAppDetails() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        val image = painterResource(R.drawable.ic_check_task)
+        Image(painter = image, contentDescription = "check")
+        Text(
+            text = "All Tasks completed",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = "Nice Work",
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TaskManagerAppPreview() {
+    TaskManagerTheme {
+        TaskManagerApp()
+    }
+}
